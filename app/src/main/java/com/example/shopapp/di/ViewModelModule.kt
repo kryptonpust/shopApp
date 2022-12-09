efa7cadf6cbcfebd0b8d68d47161e97adc5dd843
nafiul <nafiul.ice.pust@gmail.com>
2022-12-09 20:33:53 +0600

    Auth Retrofit implemented with interceptor to inject token

--- app/src/main/java/com/example/shopapp/di/ViewModelModule.kt
+++ app/src/main/java/com/example/shopapp/di/ViewModelModule.kt
@@ -1,12 +1,17 @@
 package com.example.shopapp.di
 
 import android.app.Application
+import com.example.shopapp.BuildConfig
 import com.example.shopapp.ShopApp
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.scopes.ViewModelScoped
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Named
 
 @Module
 @InstallIn(ViewModelComponent::class)
@@ -19,5 +24,29 @@ object ViewModelModule {
             return application.TOKEN
         }
         return null
+    }
+
+    @Provides
+    @ViewModelScoped
+    fun provideOkHttpClient(token:String?): OkHttpClient
+    {
+        return OkHttpClient().newBuilder().addInterceptor {
+            it.proceed(it.request()
+                .newBuilder()
+                .addHeader("Authorization", "Bearer $token")
+                .build()
+            )
+        }.build()
+    }
+
+    @Provides
+    @ViewModelScoped
+    @Named("authRetrofit")
+    fun provideAuthRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
     }
 }
